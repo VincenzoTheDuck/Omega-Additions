864bae413d464c6a0956200b006953dbf0efccec
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-11 11:04:19 -0700

    Delete scripts/items.js

--- scripts/items.js
+++
@@ -1,9 +0,0 @@
-const terraliteAlloy = extend(Item, "terralite-alloy", {});
-
-Items.serpuloItems.addAll(Items.scrap, Items.copper, Items.lead, Items.graphite, Items.coal, Items.titanium, Items.thorium,
-Items.silicon, Items.plastanium, Items.surgeAlloy, Items.sporePod, Items.phaseFabric, Items.sand, Items.blastCompound, Items.pyratite, Items.metaglass,
-terraliteAlloy);
-
-module.exports = {
-terraliteAlloy: terraliteAlloy,
-}
