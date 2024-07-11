b5e0f927bba47f82e65bc6da93c7582fa7aa8bf8
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-11 16:32:56 -0700

    Update radius.js

--- scripts/turrets/radius.js
+++ scripts/turrets/radius.js
@@ -1,4 +1,4 @@
-/*const radiusHit = new Effect(20, e => {
+const radiusHit = new Effect(20, e => {
 	Draw.color(Pal.lancerLaser);
   Lines.stroke(e.fout() * 3);
   Lines.circle(e.x, e.y, 4 + e.finpow() * 10);
@@ -11,13 +11,13 @@
   for(let i = 0; i < 4; i++){
       Drawf.tri(e.x, e.y, 15, 2 * e.fout(), i*90);
   }
-});*/
+});
 
 const radiusStar = extend(BasicBulletType, {
   damage: 100,
 	speed: 4.5,
 	lifetime: 30,
-	hitEffect: /*radiusHit*/ Fx.hitLancer,
+	hitEffect: radiusHit,
 	homingPower: 0.08,
 	despawnHit: true,
 	ammoMultiplier: 1,
@@ -39,4 +39,4 @@ const radiusStar = extend(BasicBulletType, {
 
 const radius = extend(PowerTurret, "radius", {});
 radius.range = 120;
-radius.shootType = radiusStar
+/*radius.shootType = radiusStar*/
