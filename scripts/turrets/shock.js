d020cac22bc6821b898f97c44217687153d8004e
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-12 08:27:38 -0700

    Update shock.js

--- scripts/turrets/shock.js
+++ scripts/turrets/shock.js
@@ -40,18 +40,18 @@ const shock = extend(PowerTurret, "shock", {
 	}
 });
 shock.buildType = () => extend(PowerTurret.PowerTurretBuild, shock, {
-	/*updateTile(){
+	updateTile(){
         this.super$updateTile();
 		shock.rot += (this.shootWarmup * 0.5);
 
     shock.a = Mathf.clamp(this.shootWarmup, 0, shock.maxA);
 
-        if(this.isShooting() && this.isActive() && this.hasAmmo() && Mathf.chance(this.a)){
+        if(this.isShooting() && this.isActive() && this.hasAmmo() && Mathf.chance(shock.a)){
             shockBullet.create(this, this.team, this.x, this.y, Mathf.random(360));
             Sounds.spark.at(this);
             shockShoot.at(this.x, this.y)
         }
-	};*/
+	};
 	draw(){
       Draw.rect(shock.baseRegion, this.x, this.y, 0);
       
