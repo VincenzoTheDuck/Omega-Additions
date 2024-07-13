601aff3b71310bb5fbe697f9ed32c717fbaa4fde
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-12 23:19:26 -0700

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -1,5 +1,8 @@
+const heavyAlloy = extend(Item, "heavy-alloy", {});
+
 const terraliteAlloy = extend(Item, "terralite-alloy", {});
 
 module.exports = {
+  heavyAlloy: heavyAlloy,
   terraliteAlloy: terraliteAlloy,
 }
