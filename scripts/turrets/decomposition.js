05567e301aa604e1c4a684f6535141b70230c0f0
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-14 14:24:08 -0700

    Create decomposition.js

---
+++ scripts/turrets/decomposition.js
@@ -0,0 +1,14 @@
+const missile = extend(MissileUnitType, "exothermic-missile", {
+engineLayer: Layer.effect,
+});
+
+const bullet = extend(BulletType, {
+	speed: 0,
+  keepVelocity: false,
+  spawnUnit: missile
+});
+
+const decomposition = extend(ItemTurret, "decomposition", {});
+decomposition.ammo(
+  Items.plastanium, bullet
+);
