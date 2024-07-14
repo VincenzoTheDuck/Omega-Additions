b4693854494ff555d6f9afea3282d84bec74369f
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-13 22:42:42 -0700

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -2,6 +2,8 @@ const heavyAlloy = extend(Item, "heavy-alloy", {});
 
 const terraliteAlloy = extend(Item, "terralite-alloy", {});
 
+const sulfur = extend(Item, "sulfur", {});
+
 const izomite = extend(Item, "izomite", {});
 
 const acid = extend(Liquid, "acid", {});
@@ -9,6 +11,7 @@ const acid = extend(Liquid, "acid", {});
 module.exports = {
   heavyAlloy: heavyAlloy,
   terraliteAlloy: terraliteAlloy,
+  sulfur: sulfur,
   izomite: izomite,
   acid: acid,
 }
