a62e7ee54b90f4a9d2e34894e17e6cc2177e4be5
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-15 13:58:08 -0700

    Update glow.js

--- scripts/turrets/glow.js
+++ scripts/turrets/glow.js
@@ -2,7 +2,7 @@ const bullet = extend(PointLaserBulletType, {
 	damage: 300,
 	buildingDamageMultiplier: 0.2,
   hitColor: Color.valueOf("fc8e6c"),
-  sprite: "red-point-laser"
+  sprite: "omega-adds-red-point-laser"
 });
 
 const glow = extend(ContinuousTurret, "glow", {});
