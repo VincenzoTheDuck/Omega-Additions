25ebb806b2790ff133e21929a397c8319b6a1771
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-16 11:30:19 -0700

    Update sparkle.js

--- scripts/turrets/sparkle.js
+++ scripts/turrets/sparkle.js
@@ -1,23 +1,23 @@
 const items = require("items/items")
 
-const diaHit = new Effect(40, e => {
+const diaHit = new Effect(20, e => {
 	Draw.color(Color.white);
 	Lines.stroke(e.fout() * 2)
 	
-	Lines.circle(e.x, e.y, e.fin() * 44);
+	Lines.circle(e.x, e.y, e.fin() * 20);
 
   const hj = new Floatc2({get: function(x, y){
 		var ang = Mathf.angle(x, y);
-      Lines.lineAngle(e.x + x, e.y + y, ang, e.fout() * 8);
+      Lines.lineAngle(e.x + x, e.y + y, ang, e.fout() * 4);
 	}});
 
-  Angles.randLenVectors(e.id, 9, e.finpow() * 57.0, e.rotation, 360.0, hj);
+  Angles.randLenVectors(e.id, 9, e.finpow() * 33.0, e.rotation, 360.0, hj);
 });
 
 const diaBullet = extend(MissileBulletType, {
 	damage: 45,
 	splashDamage: 95,
-  splashDamageRadius: 44,
+  splashDamageRadius: 20,
   ammoMultiplier: 3,
   homingPower: 0.09,
   weaveScl: 3.5,
