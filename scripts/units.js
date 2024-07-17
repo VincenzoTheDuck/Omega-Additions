4a0be43f0953c72e29eaaeb1159dcd7f9beff00e
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-17 11:18:07 -0700

    Update units.js

--- scripts/units.js
+++ scripts/units.js
@@ -3,3 +3,6 @@ twinkle.constructor = () => extend(UnitEntity, {});
 
 const starlight = extend(UnitType, "starlight", {});
 starlight.constructor = () => extend(UnitEntity, {});
+
+const spirit = extend(UnitType, "spirit", {});
+spirit.constructor = () => extend(UnitEntity, {});
