cc69005102354e1e1b664f4df9a776e3e9f9d261
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-19 19:18:10 -0700

    Update power.js

--- scripts/power.js
+++ scripts/power.js
@@ -1,1 +1,8 @@
+var powerProduction = 2
+
 const windTurbine = extend(PowerGenerator, "wind-turbine", {});
+windTurbine.buildType = () => extend(CoreBlock.CoreBuild, coreCage, {
+        getPowerProduction(){
+            return powerProduction;
+        }
+  });
