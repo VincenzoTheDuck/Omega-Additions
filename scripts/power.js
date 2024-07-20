44d76dbec4b3541d4b96bf7cfdbfa24b1f898509
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-19 19:21:59 -0700

    Update power.js

--- scripts/power.js
+++ scripts/power.js
@@ -1,8 +1,36 @@
-var powerProduction = 2
+//amount of power per tick * 60 ticks(1 sec.) = 540 units of power in the game per second
+var powerProduction = 2;
+//for stats
+var generationType = Stat.basePowerGeneration;
 
-const windTurbine = extend(PowerGenerator, "wind-turbine", {});
+const windTurbine = extend(PowerBlock, "wind-turbine", {
+    outputsPower: true,
+    consumesPower: false,
+	
+	//for stats
+    setStats(){
+        this.super$setStats();
+        this.stats.add(generationType, powerProduction * 60, StatUnit.powerSecond);
+    },
+	
+	//for bars
+    setBars(){
+    this.super$setBars();
+        this.addBar("poweroutput", entity => new Bar(
+	    () => Core.bundle.format("bar.poweroutput", powerProduction * 60), 
+	    () => Pal.powerBar, 
+	    () => 1
+	));
+    },
+
+    baseExplosiveness: 3,
+});
+
+//efficiency multiplier 
+const productionEfficiency = 1.0;
 windTurbine.buildType = () => extend(CoreBlock.CoreBuild, coreCage, {
+	//endowing the block with the ability to produce power
         getPowerProduction(){
-            return powerProduction;
+            return powerProduction * productionEfficiency;
         }
-  });
+});
