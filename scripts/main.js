b88a4053a2afd1de2664f14a783c438a07d60c45
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-22 17:11:43 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,11 +15,13 @@ require("turrets/arc-ii");
 require("turrets/shockgun");
 require("turrets/radius");
 require("turrets/linear");
+//TODO improve corrode and reactant
 const corrode = extend(LiquidTurret, "corrode", {});
 const reactant = extend(LiquidTurret, "reactant", {});
 require("turrets/ester");
 require("turrets/dissolve");
 require("turrets/redox");
+//TODO merge exothermic and endothermic into a single turret and add plastanium as an ammo type
 require("turrets/exothermic");
 require("turrets/endothermic");
 require("turrets/decomposition");
