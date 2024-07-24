71033cbf8463f9f7c2e63fa405f976e454ffbd19
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-24 09:18:50 -0700

    Update endothermic.js

--- scripts/turrets/endothermic.js
+++ scripts/turrets/endothermic.js
@@ -1,14 +1,25 @@
-const missile = extend(MissileUnitType, "endothermic-missile", {
+const exoMissile = extend(MissileUnitType, "exothermic-missile", {
 engineLayer: Layer.effect,
 });
 
-const bullet = extend(BulletType, {
+const exoBullet = extend(BulletType, {
 	speed: 0,
   keepVelocity: false,
-  spawnUnit: missile
+  spawnUnit: exoMissile
 });
 
-const endothermic = extend(ItemTurret, "endothermic", {});
-endothermic.ammo(
-  Items.titanium, bullet
+const endoMissile = extend(MissileUnitType, "endothermic-missile", {
+engineLayer: Layer.effect,
+});
+
+const endoBullet = extend(BulletType, {
+	speed: 0,
+  keepVelocity: false,
+  spawnUnit: endoMissile
+});
+
+const instability = extend(ItemTurret, "instability", {});
+instability.ammo(
+Items.pyratite, exoBullet,
+  Items.titanium, endoBullet,
 );
