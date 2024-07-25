845f64555c8955f63d685cd54698199fb56c729c
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-25 10:06:56 -0700

    Update liquids.js

--- scripts/items/liquids.js
+++ scripts/items/liquids.js
@@ -2,4 +2,6 @@ const acid = extend(Liquid, "acid", {});
 
 const incandescite = extend(Liquid, "incandescite", {});
 
+const cascadia = extend(Liquid, "cascadia", {});
+
 const polygen = extend(Liquid, "polygen", {});
