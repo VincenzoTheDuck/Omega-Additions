5f9d234bdbfea25462349746f113813e470e7cec
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-27 09:20:00 -0700

    Update power.js

--- scripts/power.js
+++ scripts/power.js
@@ -4,4 +4,6 @@ const fluxGenerator = extend(ConsumeGenerator, "flux-generator", {});
 
 const arcReactor = extend(ImpactReactor, "arc-reactor", {});
 
+const cascadeCollider = extend(ImpactReactor, "cascade-collider", {});
+
 const hydrogenCombustionChamber = extend(ConsumeGenerator, "hydrogen-combustion-chamber", {});
