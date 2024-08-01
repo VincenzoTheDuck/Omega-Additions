a6720bf6dc4e3f183587656bae9ec2ffe1f18c81
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-31 22:08:42 -0700

    Update periodic.js

--- scripts/turrets/periodic.js
+++ scripts/turrets/periodic.js
@@ -30,20 +30,45 @@ const perSmoke = new Effect(75, 300, e => {
   Angles.randLenVectors(e.id, 7, e.fin(Interp.pow10Out) * 29.0, e.rotation + (Time.time * 0.18), 180.0, hj);
 });
 
+const perShoot = new Effect(22, e => {
+	Draw.color(Pal.heal);
+	Lines.stroke(e.fout() * 1.25)
+
+  const hj = new Floatc2({get: function(x, y){
+		var ang = Mathf.angle(x, y);
+      Lines.lineAngle(e.x + x, e.y + y, ang, e.fout() * 5);
+	}});
+
+  Angles.randLenVectors(e.id, 5, e.finpow() * 20.0, e.rotation, 40.0, hj);
+});
+
+const perShootSmoke = new Effect(50, 300, e => {
+	Draw.color(Pal.heal);
+
+  const hj = new Floatc2({get: function(x, y){
+		var ang = Mathf.angle(x, y);
+    var rad = e.fout(Interp.pow5Out) * 3.5;
+    
+      Fill.poly(e.x + x, e.y + y, 8, rad, Time.time * -0.45);
+	}});
+  
+  Angles.randLenVectors(e.id, 7, e.fin(Interp.pow10Out) * 24.0, e.rotation, 55.0, hj);
+});
+
 const perTrail = new Effect(17, e => {
 	Draw.color(Pal.heal);
 	
 	Fill.poly(e.x, e.y, 6, e.fout(Interp.pow5Out) * 2.5, Time.time * 0.3);
 });
 
-const perIntTrail = new Effect(17, e => {
+const perIntTrail = new Effect(13, e => {
 	Draw.color(Pal.heal);
 	
 	Fill.poly(e.x, e.y, 6, e.fout(Interp.pow5Out), Time.time * 0.3);
 });
 
 const perInterval = extend(BasicBulletType, {
-  damage: 90,
+  damage: 30,
   healPercent: 1.5,
   collidesTeam: true,
 	speed: 6,
@@ -51,7 +76,7 @@ const perInterval = extend(BasicBulletType, {
 	despawnEffect: Fx.none,
   hitEffect: perShock,
 	trailEffect: perIntTrail,
-	trailInterval: 2,
+	trailInterval: 1,
 	ammoMultiplier: 1,
   homingPower: 0.04,
 	draw(b){
@@ -62,6 +87,8 @@ const perInterval = extend(BasicBulletType, {
 });
 
 const perBullet = extend(BasicBulletType, {
+	shootEffect: perShoot,
+	smokeEffect: perShootSmoke,
   damage: 90,
   healPercent: 4.5,
   collidesTeam: true,
