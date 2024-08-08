9a1fc7580c0fd6cc0fea8ce42b3d5550d5e0a5cd
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-08-08 08:09:10 -0700

    Update discharge.js

--- scripts/turrets/discharge.js
+++ scripts/turrets/discharge.js
@@ -10,7 +10,7 @@ const shootSpark = new Effect(15, e => {
   Angles.randLenVectors(e.id, 3, e.finpow() * 43.0, e.rotation, 34.0, hj);
 });
 
-const shootParticle = new Effect(15, 300, e => {
+const shootParticle = new Effect(40, 300, e => {
 	Draw.color(Color.valueOf("81e1ff"));
 
   const hj = new Floatc2({get: function(x, y){
@@ -20,7 +20,7 @@ const shootParticle = new Effect(15, 300, e => {
       Fill.circle(e.x + x, e.y + y, rad);
 	}});
 
-  Angles.randLenVectors(e.id, 3, e.fin(Interp.pow10Out) * 47.0, e.rotation, 38.0, hj);
+  Angles.randLenVectors(e.id, 1, e.fin(Interp.pow10Out) * 47.0, e.rotation, 38.0, hj);
 });
 
 const hitSpark = new Effect(30, e => {
@@ -53,6 +53,16 @@ const bullet = extend(ContinuousBulletType, {
 		if(Mathf.chance(0.2)){
 			shootParticle.at(b.x, b.y, b.rotation())
 		}
+		
+		if(b.timer(1, damageInterval)){
+            b.applyDamage(b);
+        }
+
+        if(shake > 0){
+            Effect.shake(shake, shake, b);
+        }
+
+        b.updateBulletInterval(b);
 	},
 	hitEffect: hitSpark
 });
