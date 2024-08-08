050159fa9e5c13b565a4aeeda85022013f67ce9a
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-08-08 08:18:33 -0700

    Update discharge.js

--- scripts/turrets/discharge.js
+++ scripts/turrets/discharge.js
@@ -1,28 +1,3 @@
-const shootSpark = new Effect(15, e => {
-	Draw.color(Color.white, Color.valueOf("81e1ff"), e.fin());
-	Lines.stroke(e.fout() * 2.5)
-
-  const hj = new Floatc2({get: function(x, y){
-		var ang = Mathf.angle(x, y);
-      Lines.lineAngle(e.x + x, e.y + y, ang, e.fout() * 8);
-	}});
-
-  Angles.randLenVectors(e.id, 3, e.finpow() * 43.0, e.rotation, 34.0, hj);
-});
-
-const shootParticle = new Effect(40, 300, e => {
-	Draw.color(Color.valueOf("81e1ff"));
-
-  const hj = new Floatc2({get: function(x, y){
-		var ang = Mathf.angle(x, y);
-    var rad = e.fout(Interp.pow5Out) * 2;
-    
-      Fill.circle(e.x + x, e.y + y, rad);
-	}});
-
-  Angles.randLenVectors(e.id, 1, e.fin(Interp.pow10Out) * 47.0, e.rotation, 38.0, hj);
-});
-
 const hitSpark = new Effect(30, e => {
 	Draw.color(Color.white, Color.valueOf("81e1ff"), e.fin());
 	Lines.stroke(e.fout() * 2)
